e3bf01c77f675849a9595c8a135d97014ca51849
IGOR FELIPPE SCHULLER <igor.schuller@alunos.sc.senac.br>
2025-09-23 20:53:04 -0300

    feat: search by id

--- index.js
+++ index.js
@@ -22,6 +22,16 @@ app.post('/user', async (req, res) => {
   }
 });
 
+app.get("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({where: { id: Number(id) }});
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+
 app.get('/users', async (req, res) => {
   try {
     const users = await prisma.user.findMany();
